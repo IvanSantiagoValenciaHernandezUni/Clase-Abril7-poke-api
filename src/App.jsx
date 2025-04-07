a76ca2b2aa32dbeaef9a25b51d087512bca23f9f
Iván Santiago Valencia Hernández <130880260+IvanSantiagoValenciaHernandezUni@users.noreply.github.com>
2025-04-07 20:42:51 +0000

    Cambio1

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css'
+import Aleatorios from './Componentes/Aleatorios'
+import Capturados from './Componentes/Capturados'
+import Favoritos from './Componentes/Favoritos'
+import Lista from './Componentes/Lista'
+import Pokemon from './Componentes/Pokemon'
+import Usuarios from './Componentes/Usuarios'
+import Menu from './Componentes/Menu';
+
+function App() {
+
+  return (
+    <Router>
+
+      <Menu />
+      
+      <Routes>
+        <Route path="/" element={<Aleatorios />} />
+        <Route path="/Capturados" element={<Capturados />} />
+        <Route path="/Favoritos" element={<Favoritos />} />
+        <Route path="/Lista" element={<Lista />} />
+        <Route path="/Usuarios" element={<Usuarios />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
